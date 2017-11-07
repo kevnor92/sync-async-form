bacf55027a7073e594df3b67fc16fdcb8740d164
Alumno04 J601 <a04j601@campustls.local>
2017-11-07 12:52:51 -0500

    agrego carpeta

--- public/main.js
+++ public/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function (){
-
-    $('input[type="submit"]').on("click", function(event){
-    var first_name = $('input[name="firts_name"]').val();
-    console.log (first_name);
-
+  $('input[type="submit"]').on("click", function(event){
     event.preventDefault();
+    var first_name = $('#first_name').val();
+    $.get('find.html',{'first_name':first_name});
+    $.post('find.html',{'first_name':first_name});
+  });
 });
-})
