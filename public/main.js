24d517bf56199ddb33d5439dc942cd3b0da21c65
Alumno05 J601 <a05j601@campustls.local>
2017-11-14 12:15:17 -0500

    agrego

--- public/main.js
+++ public/main.js
@@ -1,8 +1,40 @@
-$(document).ready(function (){
-  $('input[type="submit"]').on("click", function(event){
-    event.preventDefault();
-    var first_name = $('#first_name').val();
-    $.get('find.html',{'first_name':first_name});
-    $.post('find.html',{'first_name':first_name});
-  });
+  $(document).ready(function (){
+      $('input[type="submit"]').on("click", function(event){
+        event.preventDefault();
+
+        var first_name = $('#first_name').val();
+        var last_name = $('#last_name').val();
+        var gender = $('input[name="gender"]:checked').val();
+        var interest = $('input[name="interest"]:checked');
+        var interests = new Array();
+
+      for (var i = 0; i < interest.length; i++) {
+        var temp_interest = interest[i];
+        interests.push(temp_interest.value);
+      }
+
+      var payload = {
+        'first_name':first_name,
+        'last_name' :last_name,
+        'gender': gender,
+        'interests': interests
+    }
+    $.post('find.html',payload);
+    });
 });
+
+
+
+    /*var last_name = $('#last_name').val();
+    $.get('find.html',{'last_name':last_name});
+    $.post('find.html',{'last_name':last_name});
+
+  var gender = $('input[name="gender"]:checked').val();
+    $.post('find.html',{'gender':gender});
+
+
+  var interest= $('input[name="interest"]:checked').map(function(_,el){ return $(el).val();
+  }).get();
+
+    $.post('find.html',{'interest':interest});
+  /*String checkboxValues = request.getParameter("Intereses");*/
